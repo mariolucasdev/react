87c39f50a4b69508d49c70f184ab9c61fe5f609c
Mário Lucas <maryosk8bkt@gmail.com>
2017-10-11 21:09:56 -0300

    add action form

--- application/src/components/Content/Form/Form.js
+++ application/src/components/Content/Form/Form.js
@@ -2,15 +2,38 @@ import React from 'react';
 import './Form.css';
 
 class Form extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: '',
+            todo: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({value: event.target.value})
+        this.setState({todo: this.state.value});
+    }
+
+    handleSubmit(event){
+        alert('ToDo Enviado' + this.state.value)
+        event.preventDefault()
+    }
+
     render(){
         return(
             <div className="form">
                 <h3>Add ToDo</h3>
 
-                <form>
-                    <input id="todoAdd" placeholder="Adicionar Tarefa"/>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.value} onChange={this.handleChange} placeholder="Adicionar Tarefa"/>
                     <button id="btnAddItem">Adicionar</button>
                 </form>
+
+                <h1>{this.state.todo}</h1>
             </div>
         );
     }
