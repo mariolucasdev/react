85e6467fba318edd1694c56a869222fe59e367a4
Mário Lucas <maryosk8bkt@gmail.com>
2017-10-12 11:38:17 -0300

    add component Contentitens child Content

--- application/src/components/Content/Content.js
+++ application/src/components/Content/Content.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './Content.css';
 
 import Form from './Form/Form';
+import ContentItens from './ContentItens/ContentItens';
 
 class Content extends React.Component {
     render(){
         return(
-            <Form/>
+            <div>
+                <Form/>
+                <ContentItens/>
+            </div>
         );
     }
 }
---
+++ application/src/components/Content/ContentItens/ContentItens.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ContentItens.css';
+
+class ContentItens extends React.Component{
+    render(){
+        return(
+            <div>
+                <h2>Lista de Tarefas</h2>
+            </div>
+        );
+    }
+}
+
+export default ContentItens;
